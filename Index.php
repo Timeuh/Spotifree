b57b0fa8462402185fb0ff9d49d1ea47313eb9a7
Timeuh <timotdu54@gmail.com>
2022-11-28 23:26:13 +0100

    Initial commit

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Spotifree</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="icon.png">
+    <link rel="stylesheet" href="src/styles/main.css">
+</head>
+<body class="flex flex-col items-center">
+    <header class="flex flex-row items-center">
+        <img src="icon.png" alt="icone du site">
+        <h1>Bienvenue sur Spotifree</h1>
+    </header>
+    <main>
+        <h2>Ceci est le contenu</h2>
+    </main>
+    <footer>
+        <h3>Site créé par Timeuh</h3>
+    </footer>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["Index.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
