bafc19280aabb569da60ae7cf351e848da505f8e
Timeuh <timotdu54@gmail.com>
2022-11-29 22:22:25 +0100

    feat : site color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["Index.php"],
   theme: {
     extend: {},
+    colors: {
+      'prussianBlue': '#003049',
+      'maximumRed': '#D62828',
+      'orange': '#F77F00',
+      'maximumYellowRed': '#FCBF49',
+      'lemonMeringue': '#EAE2B7',
+    },
   },
   plugins: [],
 }
