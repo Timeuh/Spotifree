d659172c2d9d1359810077fa33d83b0febbd30bb
Timeuh <timotdu54@gmail.com>
2022-11-29 23:12:31 +0100

    style : changed body height to take fullscreen, background added for the header and main div content centered

--- Index.php
+++ Index.php
@@ -6,17 +6,15 @@
     <link rel="icon" href="iconPalette.png">
     <link rel="stylesheet" href="src/styles/main.css">
 </head>
-<body class="flex flex-col items-center">
-    <header class="flex flex-row items-center">
+<body class="flex flex-col items-center h-screen">
+    <header class="flex flex-row flex-wrap items-center bg-gradient-to-r from-prussianBlue via-maximumRed to-lemonMeringue justify-center w-screen h-1/6">
         <img src="iconPalette.png" alt="icone du site">
-        <h1 class="text-6xl text-red-800">Bienvenue sur Spotifree</h1>
+        <h1 class="text-6xl font-bold">Bienvenue sur Spotifree</h1>
     </header>
-    <div id="mainContainer" class="h-screen">
-        <main class="flex flex-col h-5/6 justify-center">
-            <h2 class="text-4xl">Ceci est le contenu</h2>
-        </main>
-    </div>
-    <footer class="absolute bottom-0 h-12 text-center">
+    <main class="flex h-full items-center">
+        <h2 class="text-4xl">Ceci est le contenu</h2>
+    </main>
+    <footer class="bottom-0 h-12 text-center">
         <h3 class="text-2xl">Site créé par Timeuh</h3>
     </footer>
 </body>
