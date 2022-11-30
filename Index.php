025a1f5df242fb27c3c2e87b720297954803e18e
tbrin <timotdu54@gmail.com>
2022-11-30 13:40:39 +0100

    style : the author is now colored and is a link to their github

--- Index.php
+++ Index.php
@@ -7,15 +7,15 @@
     <link rel="stylesheet" href="src/styles/main.css">
 </head>
 <body class="flex flex-col items-center h-screen">
-    <header class="flex flex-row flex-wrap items-center bg-gradient-to-r from-prussianBlue via-maximumRed to-lemonMeringue justify-center w-screen h-1/6">
+    <header class="flex flex-row flex-wrap items-center bg-gradient-to-r from-maximumRed via-maximumYellowRed to-lemonMeringue justify-center w-screen h-1/6">
         <img src="iconPalette.png" alt="icone du site">
         <h1 class="text-6xl font-bold">Bienvenue sur Spotifree</h1>
     </header>
     <main class="flex h-full items-center">
         <h2 class="text-4xl">Ceci est le contenu</h2>
     </main>
-    <footer class="bottom-0 h-12 text-center">
-        <h3 class="text-2xl">Site créé par Timeuh</h3>
-    </footer>
+    <footer class="flex flex-row space-x-4 bottom-0 h-12 text-center">
+        <h3 class="text-2xl">Site créé par</h3>
+        <a href="https://github.com/Timeuh" class="text-2xl text-lemonMeringue hover:text-prussianBlue">Timeuh</a>
 </body>
 </html>
