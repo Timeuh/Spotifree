9571a08da93ed31b3782975ab9d582f0ff673c8e
tbrin <timotdu54@gmail.com>
2022-12-10 09:14:43 +0100

    feat : update register page with a form to register people

--- src/pages/register.php
+++ src/pages/register.php
@@ -8,5 +8,18 @@
 </head>
 <body>
     <h1>Inscription</h1>
+    <form method="post" action="register.php">
+        <label>Email
+            <input type="email" placeholder="email" name="email" required>
+        </label><br>
+        <label>Mot de passe
+            <input type="password" placeholder="mot de passe" name="password" required>
+        </label><br>
+        <label>Répéter le mot de passe
+            <input type="password" placeholder="répéter le mot de passe" name="repeat" required>
+        </label><br>
+        <button type="submit">Confirmer</button>
+    </form><br><br>
+    <a href="../../index.php">Accueil</a>
 </body>
 </html>
