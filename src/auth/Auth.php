73c454047086f17786e5418bf1c7ad622e932c3a
tbrin <timotdu54@gmail.com>
2022-12-10 12:57:15 +0100

    feat : check if user exists + register user in db

--- src/auth/Auth.php
+++ src/auth/Auth.php
@@ -2,15 +2,31 @@
 
 namespace timeuh\spotifree\auth;
 
+use timeuh\spotifree\database\DBConnect;
 use timeuh\spotifree\exception\PasswordNoMatchException;
 use timeuh\spotifree\exception\PasswordNotStrongException;
 
+DBConnect::init("../../dbconfig.ini");
+
 class Auth {
 
-    public static function register(string $email, string $password, string $repeat) : bool{
+    public static function register(string $email, string $password, string $repeat) : void{
         if ($password != $repeat) throw new PasswordNoMatchException();
         if (!self::check_password_strength($password, 8)) throw new PasswordNotStrongException();
-        return true;
+
+        if (!self::check_user_registered($email)){
+            if (($db = DBConnect::makeConnection()) != null){
+                $filteredMail = filter_var($email, FILTER_SANITIZE_EMAIL);
+                $hashedPass = password_hash($password, PASSWORD_DEFAULT, ['cost'=>12]);
+
+                $insert = $db->prepare("insert into user(email,password) values (:email, :password)");
+                $insert->bindParam(':email', $filteredMail);
+                $insert->bindParam(':password', $hashedPass);
+                $insert->execute();
+
+                header("Location: ../pages/login.php?state=registered");
+            }
+        }
     }
 
     private static function check_password_strength(string $password, int $length) : bool{
@@ -20,6 +36,17 @@ class Auth {
         $lower = preg_match("#[a-z]#", $password);
         $upper = preg_match("#[A-Z]#", $password);
         if ($goodLength || !$digit || !$special || !$lower || !$upper) return false;
+        return true;
+    }
+
+    private static function check_user_registered(string $email) : bool {
+        if (($db = DBConnect::makeConnection()) != null){
+            $query = $db->prepare("select * from user where email = :email");
+            $query->bindParam(':email', $email);
+            $query->execute();
+
+            if (!$query->fetch()) return false;
+        }
         return true;
     }
 }
