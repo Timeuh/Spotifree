4af82eeef1b10e73981db1dc1e1a2bcc2d8f30ad
tbrin <timotdu54@gmail.com>
2022-12-10 13:06:54 +0100

    feat : add login form

--- src/pages/login.php
+++ src/pages/login.php
@@ -7,6 +7,21 @@
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
+    <?php
+    if (isset($_GET['state'])){
+        $state = $_GET['error'];
+        if ($state == "registered") : ?> <h1>Inscription réussie</h1><br>
+        <?php endif; } ?>
     <h1>Connexion</h1>
+    <form method="post" action="login.php">
+        <label>Email
+            <input type="email" placeholder="email" name="email" required>
+        </label><br>
+        <label>Mot de passe
+            <input type="password" placeholder="mot de passe" name="password" required>
+        </label><br>
+        <button type="submit">Connexion</button>
+    </form><br><br>
+    <a href="../../index.php">Accueil</a>
 </body>
 </html>
