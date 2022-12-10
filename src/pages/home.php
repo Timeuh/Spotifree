1339bbd8e27268501739b353a078d6cbbadac92f
tbrin <timotdu54@gmail.com>
2022-12-10 14:07:58 +0100

    feat : home page for logged user

---
+++ src/pages/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Spotifree</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="iconPalette.png">
+    <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+    <h1>Vous êtes connecté</h1>
+    <a href="../../index.php">Accueil</a>
+</body>
+</html>
