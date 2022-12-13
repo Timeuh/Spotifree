6807fde7357e249ba49294d77056ff6573b7fef3
tbrin <timotdu54@gmail.com>
2022-12-13 13:53:56 +0100

    feat : display errors in login page

--- src/pages/login.php
+++ src/pages/login.php
@@ -28,6 +28,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if ($state == "registered") : ?> <h1 class="text-4xl text-maximumRed border-2 border-maximumRed rounded-md">Inscription réussie</h1>
             <?php endif;
         } ?>
+        <?php if (isset($_GET['error'])){
+            $error = $_GET['error'];
+            if ($error == "noUser") : ?> <h1 class="text-4xl text-maximumRed text-center border-2 border-maximumRed rounded-md">Erreur : vous n'avez pas de compte, veuillez vous enregistrer</h1>
+            <?php elseif ($error == "wrongPassword") : ?> <h1 class="text-4xl text-maximumRed border-2 border-maximumRed rounded-md">Erreur : votre mot de passe ne correspond pas</h1>
+        <?php endif; } ?>
         <h1 class="text-6xl absolute top-2 font-bold">Connexion</h1>
         <form method="post" action="login.php" class="flex flex-col h-4/6 justify-center space-y-12 w-4/6 text-2xl">
             <label class="flex justify-between flex-wrap">Email
