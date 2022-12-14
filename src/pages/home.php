bc86a236d469c4c4f4b6eb8fc764c35f4cce5084
tbrin <timotdu54@gmail.com>
2022-12-14 14:07:57 +0100

    style : add tailwind style to the page

--- src/pages/home.php
+++ src/pages/home.php
@@ -6,16 +6,15 @@
     <link rel="icon" href="../../iconPalette.png">
     <link rel="stylesheet" href="../styles/main.css">
 </head>
-<body>
-    <header>
-        <h1>Vous êtes connecté</h1>
-        <a href="home.php">Explorer</a>
-        <a href="home.php">Mes playlists</a>
-        <a href="home.php">Mon profil</a>
-        <a href="../../index.php?state=disconnect">Déconnexion</a>
+<body class="h-screen bg-gradient-to-r bg-gradient-to-r from-maximumRed to-orange text-lemonMeringue">
+    <header class="text-xl flex flex-lig justify-around items-center h-12 border-b-2 rounded-b-md border-lemonMeringue bg-prussianBlue w-screen">
+        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Explorer</a>
+        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mes playlists</a>
+        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mon profil</a>
+        <a href="../../index.php?state=disconnect" class="hover:text-maximumRed">Déconnexion</a>
     </header>
-    <main>
-        <h1>Catalogue</h1>
+    <main class="flex flex-col justify-center items-center text-center h-4/6">
+        <h1 class="text-4xl">Catalogue</h1>
     </main>
     <footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center bg-prussianBlue w-screen justify-center items-center border-lemonMeringue border-t-2 rounded-t-md">
         <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
