78f956472ca7afaad21830c066208cd54cfd2e92
tbrin <timotdu54@gmail.com>
2022-12-14 13:16:06 +0100

    feat : change content of the page

--- src/pages/home.php
+++ src/pages/home.php
@@ -7,7 +7,19 @@
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
-    <h1>Vous êtes connecté</h1>
-    <a href="../../index.php?state=disconnect">Déconnexion</a>
+    <header>
+        <h1>Vous êtes connecté</h1>
+        <a href="home.php">Explorer</a>
+        <a href="home.php">Mes playlists</a>
+        <a href="home.php">Mon profil</a>
+        <a href="../../index.php?state=disconnect">Déconnexion</a>
+    </header>
+    <main>
+        <h1>Catalogue</h1>
+    </main>
+    <footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center bg-prussianBlue w-screen justify-center items-center border-lemonMeringue border-t-2 rounded-t-md">
+        <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
+        <a href="https://github.com/Timeuh" class="text-2xl text-lemonMeringue hover:text-maximumRed font-bold">Timeuh</a>
+    </footer>
 </body>
 </html>
