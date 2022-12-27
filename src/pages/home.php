023a02c15ef45c4b43b1581762d1a698cd2cf3f0
Timothee Brindejonc <timotdu54@gmail.com>
2022-12-27 18:36:08 +0100

    feat : add user profile page link

--- src/pages/home.php
+++ src/pages/home.php
@@ -7,18 +7,18 @@
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body class="h-screen bg-gradient-to-r bg-gradient-to-r from-maximumRed to-orange text-lemonMeringue">
-    <header class="text-xl flex flex-lig justify-around items-center h-12 border-b-2 rounded-b-md border-lemonMeringue bg-prussianBlue w-screen">
-        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Explorer</a>
-        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mes playlists</a>
-        <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mon profil</a>
-        <a href="../../index.php?state=disconnect" class="hover:text-maximumRed">Déconnexion</a>
-    </header>
-    <main class="flex flex-col justify-center items-center text-center h-4/6">
-        <h1 class="text-4xl">Catalogue</h1>
-    </main>
-    <footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center bg-prussianBlue w-screen justify-center items-center border-lemonMeringue border-t-2 rounded-t-md">
-        <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
-        <a href="https://github.com/Timeuh" class="text-2xl text-lemonMeringue hover:text-maximumRed font-bold">Timeuh</a>
-    </footer>
+<header class="text-xl flex flex-lig justify-around items-center h-12 border-b-2 rounded-b-md border-lemonMeringue bg-prussianBlue w-screen">
+    <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Explorer</a>
+    <a href="home.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mes playlists</a>
+    <a href="profile.php" class="hover:text-maximumRed border-r-2 border-lemonMeringue w-1/6">Mon profil</a>
+    <a href="../../index.php?state=disconnect" class="hover:text-maximumRed">Déconnexion</a>
+</header>
+<main class="flex flex-col justify-center items-center text-center h-4/6">
+    <h1 class="text-4xl">Catalogue</h1>
+</main>
+<footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center bg-prussianBlue w-screen justify-center items-center border-lemonMeringue border-t-2 rounded-t-md">
+    <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
+    <a href="https://github.com/Timeuh" class="text-2xl text-lemonMeringue hover:text-maximumRed font-bold">Timeuh</a>
+</footer>
 </body>
 </html>
