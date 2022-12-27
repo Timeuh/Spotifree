2a2de51b1121372d3518c56bbf175d69d94a8389
Timothee Brindejonc <timotdu54@gmail.com>
2022-12-27 18:32:24 +0100

    feat : add profile get and update for the user

--- src/auth/User.php
+++ src/auth/User.php
@@ -2,6 +2,8 @@
 
 namespace timeuh\spotifree\auth;
 
+use timeuh\spotifree\database\DBConnect;
+
 class User {
     private int $role;
     private string $email;
@@ -9,5 +11,34 @@ class User {
     public function __construct(int $role, string $email) {
         $this->role = $role;
         $this->email = $email;
+    }
+
+    public function getProfile(): ?array {
+        $profile = [];
+        if (($db = DBConnect::makeConnection()) != null) {
+            $query = $db->prepare("select name, surname, age, pref_style from profile where email = :email");
+            $query->bindParam(':email', $this->email);
+            $query->execute();
+
+            while ($data = $query->fetch()) {
+                $profile[0] = $data['name'];
+                $profile[1] = $data['surname'];
+                $profile[2] = $data['age'];
+                $profile[3] = $data['pref_style'];
+            }
+        }
+        return $profile;
+    }
+
+    public function updateInfos(array $profile): void {
+        if (($db = DBConnect::makeConnection()) != null) {
+            $update = $db->prepare("update profile set name= :name, surname= :surname, age= :age, pref_style= :style where email= :email");
+            $update->bindParam(':name', $profile[0]);
+            $update->bindParam(':surname', $profile[1]);
+            $update->bindParam(':age', $profile[2]);
+            $update->bindParam(':style', $profile[3]);
+            $update->bindParam(':email', $this->email);
+            $update->execute();
+        }
     }
 }
