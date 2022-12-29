e000dd5efb9e546227f7829f0643266c28e2787a
Timothee Brindejonc <timotdu54@gmail.com>
2022-12-29 15:46:43 +0100

    refactor : add divs for styling

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -35,13 +35,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 </head>
 <body>
 <main>
-    <h1>Profil</h1>
-    <h2>Nom</h2><label><?php print($profile[1]); ?></label>
-    <h2>Prenom</h2><label><?php print($profile[0]); ?></label>
-    <h2>Age</h2><label><?php print($profile[2]); ?></label>
-    <h2>Genre favori</h2><label><?php print($profile[3]); ?></label><br>
-    <a href="changeProfile.html">Modifier les informations</a><br>
-    <a href="home.php">Accueil</a>
+    <div id="container">
+        <div id="profile">
+            <h2>Nom</h2><label><?php print($profile[1]); ?></label>
+            <h2>Prenom</h2><label><?php print($profile[0]); ?></label>
+            <h2>Age</h2><label><?php print($profile[2]); ?></label>
+            <h2>Genre favori</h2><label><?php print($profile[3]); ?></label>
+        </div>
+        <div id="links">
+            <a href="changeProfile.html">Modifier les informations</a>
+            <a href="home.php">Accueil</a>
+        </div>
+    </div>
 </main>
 <footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center bg-prussianBlue w-screen justify-center items-center border-lemonMeringue border-t-2 rounded-t-md">
     <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
