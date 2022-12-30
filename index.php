0c94d565be18621ae4e713043f7096a9c60e8b75
Timeuh <timotdu54@gmail.com>
2022-12-30 21:34:03 +0100

    refactor : delete tailwind css classes to restart from zero style

--- index.php
+++ index.php
@@ -6,36 +6,36 @@
     <link rel="icon" href="icon.png">
     <link rel="stylesheet" href="src/styles/main.css">
 </head>
-<body class="flex flex-col items-center h-screen bg-gradient-to-br from-maximumRed via-maximumYellowRed to-lemonMeringue">
-<header class="flex flex-row flex-wrap items-center justify-center w-screen h-1/6 rounded-b-md border-b-2 background-plate">
+<body>
+<header>
     <img src="icon.png" alt="icone du site">
-    <h1 class="text-6xl text-lemonMeringue font-bold">Bienvenue sur Spotifree</h1>
+    <h1>Bienvenue sur Spotifree</h1>
 </header>
-<main class="flex flex-col h-full w-3/6 justify-center">
-    <div class="flex flex-col text-lemonMeringue text-center space-y-8 h-4/6 justify-center items-center rounded-md border-2 background-plate">
+<main>
+    <div>
         <?php
             session_start();
             if($_SERVER['REQUEST_METHOD'] == "GET"){
                 if (isset($_GET['state'])){
-                    if ($_GET['state'] == "disconnect") : ?><h1 class="text-3xl text-green-600">Déconnexion réussie</h1>
-            <?php
+                    if ($_GET['state'] == "disconnect") : ?><h1>Déconnexion réussie</h1>
+                        <?php
                         session_destroy();
                     endif;
                     }
                 } ?>
-        <h2 class="text-4xl">Pas encore de compte ?</h2>
-        <a href="src/pages/register.php" class="button-default">
-            <span class="button-content">S'inscrire</span>
+        <h2>Pas encore de compte ?</h2>
+        <a href="src/pages/register.php">
+            <span>S'inscrire</span>
         </a>
-        <h2 class="text-4xl">Déjà un compte ?</h2>
-        <a href="src/pages/login.php" class="button-default">
-            <span class="button-content">Se connecter</span>
+        <h2>Déjà un compte ?</h2>
+        <a href="src/pages/login.php">
+            <span>Se connecter</span>
         </a>
     </div>
 </main>
-<footer class="flex flex-row space-x-2 bottom-0 h-12 text-center w-screen justify-center items-center border-t-2 rounded-t-md background-plate">
-    <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
-    <a href="https://github.com/Timeuh" class="text-2xl text-maximumRed hover:text-prussianBlue font-bold">Timeuh</a>
+<footer>
+    <h3>Site créé par</h3>
+    <a href="https://github.com/Timeuh">Timeuh</a>
 </footer>
 </body>
 </html>
--- src/pages/login.php
+++ src/pages/login.php
@@ -20,43 +20,41 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <link rel="icon" href="../../icon.png">
     <link rel="stylesheet" href="../styles/main.css">
 </head>
-<body class="flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-r from-maximumRed via-maximumYellowRed to-lemonMeringue">
-<main class="flex flex-col h-5/6 w-3/6 items-center justify-center relative background-plate border-2 border-prussianBlue rounded-md text-lemonMeringue">
+<body>
+<main>
     <?php
         if (isset($_GET['state'])) {
             $state = $_GET['state'];
-            if ($state == "registered") : ?> <h1 class="text-4xl text-green-600">Inscription réussie</h1>
+            if ($state == "registered") : ?> <h1>Inscription réussie</h1>
             <?php endif;
         } ?>
         <?php if (isset($_GET['error'])) {
             $error = $_GET['error'];
-            if ($error == "noUser") : ?> <h1 class="text-4xl text-maximumRed text-center pt-12">Erreur : vous n'avez pas
-                de compte, veuillez vous enregistrer</h1>
-            <?php elseif ($error == "wrongPassword") : ?> <h1 class="text-4xl text-maximumRed text-center pt-12">Erreur
-                : votre mot de passe ne correspond pas</h1>
+            if ($error == "noUser") : ?> <h1>Erreur : vous n'avez pas de compte, veuillez vous enregistrer</h1>
+            <?php elseif ($error == "wrongPassword") : ?> <h1>Erreur : votre mot de passe ne correspond pas</h1>
             <?php endif;
         } ?>
-    <h1 class="text-6xl absolute top-2 font-bold">Connexion</h1>
-    <form method="post" action="login.php" class="flex flex-col h-4/6 justify-center space-y-12 w-4/6 text-2xl">
-        <label class="flex justify-between flex-wrap">Email
-            <input type="email" placeholder="email" name="email" required class="form-content">
+    <h1>Connexion</h1>
+    <form method="post" action="login.php">
+        <label>Email
+            <input type="email" placeholder="email" name="email" required>
         </label>
-        <label class="flex justify-between flex-wrap">Mot de passe
-            <input type="password" placeholder="mot de passe" name="password" required class="form-content">
+        <label>Mot de passe
+            <input type="password" placeholder="mot de passe" name="password" required>
         </label>
-        <div class="text-center">
-            <button type="submit" class="button-default">
-                <span class="button-content">Connexion</span>
+        <div>
+            <button type="submit">
+                <span>Connexion</span>
             </button>
         </div>
     </form>
-    <a href="../../index.php" class="button-default">
-        <span class="button-content">Accueil</span>
+    <a href="../../index.php">
+        <span>Accueil</span>
     </a>
 </main>
-<footer class="absolute bottom-0 flex flex-row space-x-2 bottom-0 h-12 text-center w-screen justify-center items-center border-t-2 rounded-t-md background-plate">
-    <h3 class="text-2xl text-lemonMeringue">Site créé par</h3>
-    <a href="https://github.com/Timeuh" class="text-2xl text-maximumRed hover:text-prussianBlue font-bold">Timeuh</a>
+<footer>
+    <h3>Site créé par</h3>
+    <a href="https://github.com/Timeuh">Timeuh</a>
 </footer>
 </body>
 </html>
