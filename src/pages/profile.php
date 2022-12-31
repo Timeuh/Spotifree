89b18153abc4abec2698e54a52ea28acc7d0fea8
Timeuh <timotdu54@gmail.com>
2022-12-31 16:15:05 +0100

    style : add style to profile page

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -33,28 +33,31 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <link rel="icon" href="../../icon.png">
     <link rel="stylesheet" href="../styles/main.css">
 </head>
-<body>
-<main>
-    <div id="container">
-        <div id="profile">
-            <h2>Nom</h2><label><?php print($profile[1]); ?></label>
-            <h2>Prenom</h2><label><?php print($profile[0]); ?></label>
-            <h2>Age</h2><label><?php print($profile[2]); ?></label>
-            <h2>Genre favori</h2><label><?php print($profile[3]); ?></label>
+<body class="background h-screen w-screen justify-center items-center flex text-burnt-sienna-200">
+<main class="flex flex-col background-plate justify-center items-center h-5/6 w-3/5 rounded-md">
+    <div id="profile" class="space-y-12 text-3xl text-center font-bold">
+        <div id="user" class="grid grid-cols-3 justify-self-center items-center">
+            <img alt="icone du site" class="object-none h-40 w-40" src="../img/profile.png">
+            <label><?php print($profile[0]); ?></label>
+            <label><?php print($profile[1]); ?></label>
+        </div>
+        <div id="age" class="grid grid-cols-2 justify-self-center items-center">
+            <img alt="icone du site" class="object-none h-40 w-40" src="../img/birthday.png">
+            <label><?php print($profile[2]); ?></label>
+        </div>
+        <div id="favorite" class="grid grid-cols-2 justify-self-center items-center">
+            <img alt="icone du site" class="object-none h-40 w-40" src="../img/heart.png">
+            <label><?php print($profile[3]); ?></label>
         </div>
     </div>
-    <div id="links">
-        <a href="changeProfile.html">
-            <span>Modifier les informations</span>
-        </a>
-        <a href="home.html">
-            <span>Accueil</span>
-        </a>
+    <div id="links" class="grid grid-cols-2 w-full text-center text-3xl pt-12 mt-12">
+        <a href="changeProfile.html" class="link">Modifier les informations</a>
+        <a href="home.html" class="link">Accueil</a>
     </div>
 </main>
-<footer>
-    <h3>Site créé par</h3>
-    <a href="https://github.com/Timeuh">Timeuh</a>
+<footer class="fixed bottom-0 flex flex-lig w-screen justify-center text-3xl py-4">
+    <h3 class="px-2">Site créé par</h3>
+    <a class="link" href="https://github.com/Timeuh">Timeuh</a>
 </footer>
 </body>
 </html>
