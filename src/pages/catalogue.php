1e059fa31b4f9cb761e85062e297bd62e95e62e6
Timothee Brindejonc <timotdu54@gmail.com>
2023-01-13 14:03:02 +0100

    feat: add link to catalogue page

--- src/pages/catalogue.php
+++ src/pages/catalogue.php
@@ -2,14 +2,16 @@
 
 use timeuh\spotifree\audio\Playlist;
 use timeuh\spotifree\audio\Track;
+use timeuh\spotifree\database\DBConnect;
 use timeuh\spotifree\render\PlaylistRenderer;
 
 session_start();
 require_once "../../vendor/autoload.php";
+DBConnect::init("../../dbconfig.ini");
 
 $catalogue = new Playlist(0, 0, "Catalogue");
 
-for ($i = 0; $i < 11; $i++) {
+for ($i = 1; $i < 11; $i++) {
     $track = Track::findById($i);
     $catalogue->addTrack($track);
 }
