bc9f2a853aa611b990a88a01503692e82b6c4427
Timothee Brindejonc <timotdu54@gmail.com>
2023-01-17 13:29:23 +0100

    feat: display playlists of user and links to create new ones

--- src/pages/userPLaylists.php
+++ src/pages/userPLaylists.php
@@ -5,6 +5,9 @@ use timeuh\spotifree\database\DBConnect;
 session_start();
 require_once "../../vendor/autoload.php";
 DBConnect::init("../../dbconfig.ini");
+
+$user = unserialize($_SESSION['user']);
+$playlists = $user->findPlaylists();
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +21,21 @@ DBConnect::init("../../dbconfig.ini");
 <body class="h-screen w-screen background">
 <main>
     <a href="home.html">Accueil</a>
+    <?php
+    if ($playlists == []) {
+        print ("<h1>Vous n'avez pas de playlist, créez-en une !</h1>");
+    } else {
+        foreach ($playlists as $key => $value) {
+            foreach ($value as $container => $playlist) {
+                $id = $playlist[0];
+                $title = $playlist[1];
+
+                print ("<a href='displayPlaylist.php?id=" . $id . "'>" . $title . "</a>");
+            }
+        }
+    }
+    print("<a href='createPlaylist.php'>Créer une playlist</a>");
+    ?>
 </main>
 <footer class="fixed bottom-0 flex flex-lig w-full justify-center text-3xl py-4">
     <h3 class="px-2">Site créé par</h3>
