e0e8b55e3225dba11a608f2297f22ab14848ac14
Timeuh <timotdu54@gmail.com>
2023-01-30 08:26:04 +0100

    style: change track title color

--- src/render/TrackRenderer.php
+++ src/render/TrackRenderer.php
@@ -17,7 +17,7 @@ class TrackRenderer {
         $minutes = floor($duration / 60);
 
         return "<div class='flex flex-col justify-center items-center py-12 text-xl'>
-                <h1 class='text-3xl text-burnt-sienna-300'>$title</h1>
+                <h1 class='text-3xl text-burnt-sienna-500'>$title</h1>
                 <p>Par <b class='text-lavender-300'>$artist</b>, Durée <b class='text-lavender-300'>$minutes:$seconds</b></p> 
                 <audio class='pt-4' src='../music/$filename.mp3' controls></audio>
                 <a href='addPlaylist.php?track=" . $id . "' class='link'>+ Playlist</a> 
